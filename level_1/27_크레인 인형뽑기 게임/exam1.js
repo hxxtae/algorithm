8623c2bdea193c79cf993d12fba3423ff4ffb2be
Heetae Kim <fkdlxmfkdl@gmail.com>
2022-12-25 23:20:48 +0900

    :book: [feat]: Add Exam 27
    - Add Exam 27 (level: 1)

---
+++ level_1/27_크레인 인형뽑기 게임/exam1.js
@@ -0,0 +1,30 @@
+// 1.
+function solution(board, moves) {
+  let count = 0;
+  const stack = [];
+  const yLen = board.length;
+  for(let i = 0, len = moves.length; i < len; i++) {
+    const x = moves[i] - 1;
+    for(let y = 0; y < yLen; y++) {
+      const item = board[y][x];
+      if(item) {
+        stack.push(item);
+        board[y][x] = 0;
+        break;
+      }
+    }
+    if(stack.length > 1) {
+      if(stack.at(-1) === stack.at(-2)) {
+        stack.pop();
+        stack.pop();
+        count += 2;
+      }
+    }
+  }
+  return count;
+}
+
+// NOTE: 스택 사용
+
+// 2. 다르게 풀이
+
