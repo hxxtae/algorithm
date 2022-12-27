a33c97e07de3ac1e67a8d36208977e0a5187a63d
Heetae Kim <fkdlxmfkdl@gmail.com>
2022-12-27 23:55:38 +0900

    :book: [feat]: Add Exam 31
    - Add Exam 31 (level: 1)

---
+++ level_1/31_K번째수/exam1.js
@@ -0,0 +1,9 @@
+function solution(array, commands) {
+  const result = [];
+  for(let i = 0, len = commands.length; i < len; i++) {
+      const [start, end, n] = commands[i];
+      const num = array.slice(start - 1, end).sort((a, b) => a - b)[n - 1];
+      result.push(num);
+  }
+  return result;
+}
