af0daf1ff9fd7aafb98ecf8605b7ef5189bf3ac1
hxxtae <fkdlxmfkdl@gmail.com>
2023-10-28 21:41:20 +0900

    :books: [Docs]: Add "BFS" to Exam 34

--- boj_dfs_bfs/34_양/exam.js
+++ boj_dfs_bfs/34_양/exam.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const stdin = fs.readFileSync('./input_3.txt').toString().trim();
+const stdin = fs.readFileSync('./input_1.txt').toString().trim();
 
 // -------------
 // 입력
@@ -54,9 +54,57 @@ function solution1(r, c, yard) {
 // -------------
 // 풀이 (BFS)
 // -------------
+function solution2(r, c, yard) {
+  const visited = Array.from({ length: r }, () => Array(c).fill(0));
+
+  const findWay = (y, x, way) => {
+    const X = [1, 0, -1, 0];
+    const Y = [0, 1, 0, -1];
+    return [y + Y[way], x + X[way]];
+  }
+
+  const bfs = (startY, startX) => {
+    const queue = [[startY, startX]];
+    visited[startY][startX] = 1;
+
+    let sheep = 0, wolf = 0;
+    while (queue.length) {
+      const [y, x] = queue.shift();
+      yard[y][x] === 'v' && wolf++;
+      yard[y][x] === 'o' && sheep++;
+
+      for (let i = 0; i < 4; i++) {
+        const [nextY, nextX] = findWay(y, x, i);
+        if (nextY >= 0 && nextX >= 0 && nextY < r && nextX < c) {
+          if (yard[nextY][nextX] !== '#' && !visited[nextY][nextX]) {
+            visited[nextY][nextX] = 1;
+            queue.push([nextY, nextX]);
+          }
+        }
+      }
+    }
+    if (sheep > wolf) wolf = 0;
+    else sheep = 0;
+    return [sheep, wolf];
+  }
+
+  const answer = [0, 0]; // [total_sheep_count, total_wolf_count]
+  for (let row = 0; row < r; row++) {
+    for (let col = 0; col < c; col++) {
+      if (yard[row][col] !== '#' && !visited[row][col]) {
+        const [sheepCnt, wolfCnt] = bfs(row, col);
+        answer[0] += sheepCnt;
+        answer[1] += wolfCnt;
+      }
+    }
+  }
+  return answer.join(' ');
+}
 
 // -------------
 // 출력
 // -------------
 const result1 = solution1(R, C, YARD);
+const result2 = solution2(R, C, YARD);
 console.log(result1);
+console.log(result2);
