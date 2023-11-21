dbd284629ea7207082706dc270ef3b45fd9f9027
hxxtae <fkdlxmfkdl@gmail.com>
2023-11-21 15:51:17 +0900

    :books: [Docs]: Update Exam 10
    - The Exam 10 has been regraded
      (No.1984 - 알파벳)

--- boj_dfs_bfs/10_알파벳/exam.js
+++ boj_dfs_bfs/10_알파벳/exam.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const stdin = fs.readFileSync('./input_2.txt').toString().trim();
+const stdin = fs.readFileSync('./input_3.txt').toString().trim();
 
 // -------------
 // 입력
@@ -11,9 +11,10 @@ const [[R, C], ...BOARD] = input;
 // 풀이 (DFS)
 // -------------
 function solution1(r, c, board) {
-  const visited = {};
+  const visited = Array.from({ length: r }, () => Array(c).fill(0));
+  const visitedObj = {};
   for (let i = 0; i < 26; i++) {
-    visited[String.fromCharCode(i + 65)] = 0;
+    visitedObj[String.fromCharCode(i + 65)] = 0;
   }
   
   const findWay = (y, x, way) => {
@@ -26,10 +27,12 @@ function solution1(r, c, board) {
     for (let i = 0; i < 4; i++) {
       const [nextY, nextX] = findWay(nowY, nowX, i);
       if (nextY >= 0 && nextX >= 0 && nextY < r && nextX < c) {
-        if (!visited[board[nextY][nextX]]) {
-          visited[board[nextY][nextX]] = 1;
-          dfs(nextY, nextX, count+1);
-          visited[board[nextY][nextX]] = 0;
+        if (!visited[nextY][nextX] && !visitedObj[board[nextY][nextX]]) {
+          visited[nextY][nextX] = 1;
+          visitedObj[board[nextY][nextX]] = 1;
+          dfs(nextY, nextX, count + 1);
+          visited[nextY][nextX] = 0;
+          visitedObj[board[nextY][nextX]] = 0;
         }
       }
     }
@@ -38,7 +41,8 @@ function solution1(r, c, board) {
   }
 
   let result = 1;
-  visited[board[0][0]] = 1;
+  visited[0][0] = 1;
+  visitedObj[board[0][0]] = 1;
   dfs(0, 0, result);
   return result;
 }
@@ -92,7 +96,8 @@ console.log(result2);
 
 // [접근]
 // DFS를 통해 Board를 탐색하면서 이전에 탐색한 알파벳의 경우 방문 여부를 기록해 주어야 한다.
-// visited를 객체나 Hash Map을 통해 해당 알파벳(key)에 대한 방문 여부(value)를 기록해 주면서
+// visitedObj를 객체나 Hash Map을 통해 해당 알파벳(key)에 대한 방문 여부(value)를 기록해 주면서
 // 깊이 우선 탐색을 수행하도록 한다.
+// 동시에 visited 배열을 통해 이전에 방문한 노드를 다시 방문하지 않도록 한다.
 
 // ※ 해시 객체의 경우 리터럴 객체보다 느린 자료구조이기 때문에 BOJ에서 시간 초과가 발생한다.
