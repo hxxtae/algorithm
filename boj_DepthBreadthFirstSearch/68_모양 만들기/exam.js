2b4dc94f1a8f2ed851d128f7fe30be1e3d8766bb
hxxtae <fkdlxmfkdl@gmail.com>
2024-03-10 22:47:38 +0900

    :books: [Docs]: Update Exam 68
    - Update Exam 68 (dfs/bfs)

--- boj_DepthBreadthFirstSearch/68_모양 만들기/exam.js
+++ boj_DepthBreadthFirstSearch/68_모양 만들기/exam.js
@@ -57,24 +57,24 @@ function solution(n, m, matrix) {
   }
 
   // 1.
-  let oneOfKey = 1;
+  let arrKey = 1;
   for (let r = 0; r < n; r++) {
     for (let c = 0; c < m; c++) {
       if (matrix[r][c] === "1" && visited[r][c] === -1) {
         // 1-1. DFS
-        // visited[r][c] = [oneOfKey];
+        // visited[r][c] = [arrKey];
         // const count = dfs(r, c, 1, visited[r][c]);
 
         // 1-1. BFS
-        bfs(r, c, [oneOfKey]);
-        oneOfKey += 1;
+        bfs(r, c, [arrKey]);
+        arrKey += 1;
       }
     }
   }
 
   // 2-1.
   const onShapeCount = (y, x, cnt) => {
-    const oneList = [];
+    const oneList = []; // new Set으로 바꾸어도 좋을듯 하다.
     for (let i = 0; i < 4; i++) {
       const [aroundY, aroundX] = findWay(y, x, i);
       if (aroundY < 0 || aroundX < 0 || aroundY >= n || aroundX >= m) continue;
@@ -110,4 +110,19 @@ const result = solution(N, M, MATRIX);
 console.log(result);
 
 // [접근]
+// 문제에서 요구하는 바는 1로 변을 인접하고 있는 경우 연결이 가능하며 연결된 1의 개수를 모양의 크기라고 한다.
+// 그런데 배열에서 칸 하나에 들어있는 수를 변경해서 만들 수 있는 최대 크기를 구해야 한다.
+// 즉, 연결된 1의 모양에서 주변에 인접한 0 하나만 1로 바꾸어 연결하였을 경우 최대가 되는 모양의 크기를 구하면 된다.
+
+// 1. 먼저 1로 연결된 모양의 크기를 구한다. 모양은 구분지어 놓는다.
+
+// - 탐색을 통해 연결된 1을 카운트 하여 모양을 찾는다.
+// - visited의 값 : [모양구분key, 모양크기count]
+
+// 2. 배열에서 0인 경우를 탐색하여 인접한 곳에 모양을 가진 경우가 존재하면 모양의 크기를 더해준다.
+
+// - 0을 1로 바꾸면 크기 (1) + 인접한 모양의 크기 (0 ~ ?)
+// - 최대 모양의 크기를 반환
+
+// ### 메모
 // 문제의 접근과 유형 모두 "38_벽 부수고 이동하기4" 문제와 아주 비슷하다.
